f6644e3f63ce5b6079eec9a09308d4f7987093f5
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2014-09-18 14:51:16 +0900

    Initial Commit

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by 斉藤 祐輔 on 2014/09/18.
+//  Copyright (c) 2014年 JIBUNSTYLE Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
